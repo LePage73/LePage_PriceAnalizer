3b8bb955be344a7af5824a437c1a071a4d32af67
LePage73 <lepage@simbirsk.ru>
2024-11-12 12:36:08 +0400

    сдаю на проверку

--- project.py
+++ project.py
@@ -1,38 +1,59 @@
+# Практическое задание "Анализатор прайс-листов."
 import os
-import json
+import pandas as pd
 
 
 class PriceMachine():
-    
     def __init__(self):
-        self.data = []
-        self.result = ''
-        self.name_length = 0
-    
+        self.data = pd.DataFrame()
+        self.list_files_ = []
+        self.file_path = ''
+
     def load_prices(self, file_path=''):
         '''
             Сканирует указанный каталог. Ищет файлы со словом price в названии.
             В файле ищет столбцы с названием товара, ценой и весом.
             Допустимые названия для столбца с товаром:
-                товар
-                название
-                наименование
-                продукт
-                
+                товар, название, наименование, продукт
             Допустимые названия для столбца с ценой:
-                розница
-                цена
-                
+                розница, цена
             Допустимые названия для столбца с весом (в кг.)
-                вес
-                масса
-                фасовка
+                вес, масса, фасовка
         '''
-        
-    def _search_product_price_weight(self, headers):
-        '''
-            Возвращает номера столбцов
-        '''
+        self.file_path = file_path
+        # ищем имена файлов с прайсами
+        for root, dirs, files in os.walk(self.file_path):
+            for file in files:
+                if file.endswith('.csv') and 'price' in file:
+                    self.list_files_.append(file)
+        print('обнаружены файлы прайс-листов')
+        _ = [print(file) for file in self.list_files_]
+        # читаем данные из каждого файла и помещаем в датафрейм
+        for filename in self.list_files_:
+            df = pd.read_csv(file_path + filename, encoding='UTF-8', delimiter=',')
+            # переименовываем столбцы под один шаблон
+            df.rename(columns={'название': 'Product',
+                               'продукт': 'Product',
+                               'товар': 'Product',
+                               'наименование': 'Product',
+                               'цена': 'Price',
+                               'розница': 'Price',
+                               'фасовка': 'Wght',
+                               'масса': 'Wght',
+                               'вес': 'Wght'
+                               }, inplace=True)
+            # указываем из какого файла запись
+            df['File'] = filename
+            # оставляем только нужные столбцы
+            df = df[['Product', 'Price', 'Wght', 'File']]
+            # создаем столбец с ценой за килограмм
+            df['Prc_kg'] = round(df['Price'] / df['Wght'], ndigits=2)
+            # добавляем прайс к общему прайс-листу
+            self.data = pd.concat([self.data, df], ignore_index=True)
+        # отсортируем по возрастанию цены за кг
+        self.data = self.data.sort_values('Prc_kg', ascending=True, ignore_index=True)
+        print(' файлы прочитаны, вся информация занесена в общий прайс-лист, отсортирована')
+        print(self.data)
 
     def export_to_html(self, fname='output.html'):
         result = '''
@@ -42,25 +63,69 @@ class PriceMachine():
             <title>Позиции продуктов</title>
         </head>
         <body>
-            <table>
-                <tr>
-                    <th>Номер</th>
-                    <th>Название</th>
-                    <th>Цена</th>
-                    <th>Фасовка</th>
-                    <th>Файл</th>
-                    <th>Цена за кг.</th>
-                </tr>
         '''
-    
-    def find_text(self, text):
+        pd.set_option('display.max_colwidth', None)
+        # переименуем столбцы на русский
+        data_frame = self.data.rename(columns={'Product': 'Название',
+                                               'Price': 'Цена',
+                                               'Wght': 'Фасовка',
+                                               'File': 'Файл',
+                                               'Prc_kg': 'Цена за кг.',
+                                               })
+        table_html = data_frame.to_html()
+        footer = '''
+        </body>
+        </html>
+        '''
+        fname = self.file_path + fname
+        with open(fname, 'w', encoding='utf-8') as file:
+            file.write(result + table_html + footer)
+            file.close()
+        pass
 
-    
-pm = PriceMachine()
-print(pm.load_prices())
+    def find_text(self, text):
+        '''
+        ищем продукты в датафрейме содержащие в названии text
+        '''
+        print('-------------------------- РЕЗУЛЬТАТ ЗАПРОСА ---------------------------')
+        # отбираем строки где в названии продукта есть text
+        resultat = self.data.loc[self.data.apply(lambda x: text.lower() in x['Product'].lower(), axis=1)]
+        with pd.option_context('display.max_rows', None, 'display.max_columns', 7, 'display.max_colwidth', 40):
+            print(resultat.sort_values('Prc_kg', ascending=True, ignore_index=True))
+        print('------------------------------------------------------------------------')
+        pass
 
 '''
     Логика работы программы
 '''
-print('the end')
-print(pm.export_to_html())
+pm = PriceMachine()
+while True:
+    file_path = input('Укажите путь к папке с файлами прайс-листов >')
+    if os.path.exists(file_path):
+        break
+    print(f'Путь {file_path} не найден')
+pm.load_prices(file_path=file_path)
+
+while True:
+    enter = input('\nУкажите часть названия продукта или exit для выхода >')
+    if enter.lower() == 'exit':
+        break
+    pm.find_text(text=enter)
+print('the end - работа анализатора завершена')
+
+while True:
+    html_name = input(
+        '\nДля сохранения общего прайса в HTML, задайте имя файла, \nесли имя пустое файл не будет создан >')
+    html_name = str(html_name)
+    if len(html_name) != 0:
+        if not '.html' in html_name and not '.htm' in html_name:
+            html_name = html_name + '.html'
+        try:
+            pm.export_to_html(fname=html_name)
+            break
+        except:
+            print(f'Неверное имя {html_name}, файл не создан')
+            continue
+    break
+
+print('Программа завершена')
